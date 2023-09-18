004230910c43915bd8e40cb3178f9ec49e41f287
chamlingsuman <mack30324@gmail.com>
2023-09-18 17:44:16 +0545

    add another excersice

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ function palindrome(str) {
   }
   
  palindrome("eye");
+
+
+ //Roman Numeral Converter
+ function convertToRoman(num) {
+  //CREATE ROMAN NUMERAL TO NUMBER LOOKUP TABLE
+  const lookupTable = {
+    M : 1000,
+    CM : 900,
+    D : 500,
+    CD : 400,
+    C : 100,
+    XC : 90,
+    L : 50,
+    XL: 40,
+    X : 10,
+    IX : 9,
+    V : 5,
+    IV : 4,
+    I : 1
+  };
+// CREATE ROMAN NUMERAL ACCUMULATOR
+let accumulator = '';
+
+// LOOP THROUGH LOOKUP TABLE
+for(const key in lookupTable){
+  const numberValue = lookupTable[key];
+  //console.log(key, numberValue);
+//IF CURRENTNUMBER  <= NUM THEN SUBTRACT CURRENTNUMBER FROM NUM. ADD SYMBOL TO ACCUMLATOR.
+ while(numberValue <= num){
+  num -= numberValue;
+  accumulator += key;
+ }
+
+}
+//RETURN ACCUMULATOR
+  return accumulator
+ }
+ 
+ convertToRoman(36);
+
+
+
+ //
